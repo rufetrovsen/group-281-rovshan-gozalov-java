3269e1b593133f571c4dde737d4ca4ff3097310b
rufetrovsen225 <talibovag@gmail.com>
2026-02-04 22:44:05 +0400

    homework_5

---
+++ src/homework_5/main.java
@@ -0,0 +1,231 @@
+package homework_5;
+
+public class main {
+	
+	        Dog dog = new Dog();
+	        dog.bark();
+	        dog.run();
+
+	        Cat cat = new Cat();
+	        cat.meow();
+	        cat.sleep();
+
+	        Lion lion = new Lion();
+	        lion.roar();
+	        lion.hunt();
+
+	        Tiger tiger = new Tiger();
+	        tiger.growl();
+	        tiger.jump();
+
+	        Elephant elephant = new Elephant();
+	        elephant.trumpet();
+	        elephant.sprayWater();
+
+	        Horse horse = new Horse();
+	        horse.neigh();
+	        horse.gallop();
+
+	        Cow cow = new Cow();
+	        cow.moo();
+	        cow.giveMilk();
+
+	        Sheep sheep = new Sheep();
+	        sheep.baa();
+	        sheep.giveWool();
+
+	        Goat goat = new Goat();
+	        goat.bleat();
+	        goat.climb();
+
+	        Rabbit rabbit = new Rabbit();
+	        rabbit.hop();
+	        rabbit.eatCarrot();
+
+	        Fox fox = new Fox();
+	        fox.scream();
+	        fox.sneak();
+
+	        Wolf wolf = new Wolf();
+	        wolf.howl();
+	        wolf.leadPack();
+
+	        Bear bear = new Bear();
+	        bear.growl();
+	        bear.catchFish();
+
+	        Panda panda = new Panda();
+	        panda.eatBamboo();
+	        panda.roll();
+
+	        Monkey monkey = new Monkey();
+	        monkey.climbTree();
+	        monkey.throwBanana();
+
+	        Giraffe giraffe = new Giraffe();
+	        giraffe.eatLeaves();
+	        giraffe.stretchNeck();
+
+	        Zebra zebra = new Zebra();
+	        zebra.run();
+	        zebra.hideInStripes();
+
+	        Kangaroo kangaroo = new Kangaroo();
+	        kangaroo.jump();
+	        kangaroo.carryBaby();
+
+	        Dolphin dolphin = new Dolphin();
+	        dolphin.swim();
+	        dolphin.doTrick();
+
+	        Shark shark = new Shark();
+	        shark.bite();
+	        shark.smellBlood();
+
+	        Whale whale = new Whale();
+	        whale.sing();
+	        whale.diveDeep();
+
+	        Eagle eagle = new Eagle();
+	        eagle.fly();
+	        eagle.hunt();
+
+	        Owl owl = new Owl();
+	        owl.hoot();
+	        owl.seeAtNight();
+
+	        Snake snake = new Snake();
+	        snake.hiss();
+	        snake.bite();
+
+	        Crocodile crocodile = new Crocodile();
+	        crocodile.snap();
+	        crocodile.swim();
+	    }
+	}
+
+	class Dog {
+	    void bark() { System.out.println("Dog: Woof!"); }
+	    void run() { System.out.println("Dog runs fast"); }
+	}
+
+	class Cat {
+	    void meow() { System.out.println("Cat: Meow!"); }
+	    void sleep() { System.out.println("Cat sleeps"); }
+	}
+
+	class Lion {
+	    void roar() { System.out.println("Lion: ROAR!"); }
+	    void hunt() { System.out.println("Lion hunts"); }
+	}
+
+	class Tiger {
+	    void growl() { System.out.println("Tiger: Grrr!"); }
+	    void jump() { System.out.println("Tiger jumps"); }
+	}
+
+	class Elephant {
+	    void trumpet() { System.out.println("Elephant: Prrrr!"); }
+	    void sprayWater() { System.out.println("Elephant sprays water"); }
+	}
+
+	class Horse {
+	    void neigh() { System.out.println("Horse: Neigh!"); }
+	    void gallop() { System.out.println("Horse gallops"); }
+	}
+
+	class Cow {
+	    void moo() { System.out.println("Cow: Moo!"); }
+	    void giveMilk() { System.out.println("Cow gives milk"); }
+	}
+
+	class Sheep {
+	    void baa() { System.out.println("Sheep: Baa!"); }
+	    void giveWool() { System.out.println("Sheep gives wool"); }
+	}
+
+	class Goat {
+	    void bleat() { System.out.println("Goat: Meee!"); }
+	    void climb() { System.out.println("Goat climbs rocks"); }
+	}
+
+	class Rabbit {
+	    void hop() { System.out.println("Rabbit hops"); }
+	    void eatCarrot() { System.out.println("Rabbit eats carrot"); }
+	}
+
+	class Fox {
+	    void scream() { System.out.println("Fox: Ring-ding-ding!"); }
+	    void sneak() { System.out.println("Fox sneaks quietly"); }
+	}
+
+	class Wolf {
+	    void howl() { System.out.println("Wolf: Auuuuu!"); }
+	    void leadPack() { System.out.println("Wolf leads the pack"); }
+	}
+
+	class Bear {
+	    void growl() { System.out.println("Bear growls"); }
+	    void catchFish() { System.out.println("Bear catches fish"); }
+	}
+
+	class Panda {
+	    void eatBamboo() { System.out.println("Panda eats bamboo"); }
+	    void roll() { System.out.println("Panda rolls"); }
+	}
+
+	class Monkey {
+	    void climbTree() { System.out.println("Monkey climbs tree"); }
+	    void throwBanana() { System.out.println("Monkey throws banana"); }
+	}
+
+	class Giraffe {
+	    void eatLeaves() { System.out.println("Giraffe eats leaves"); }
+	    void stretchNeck() { System.out.println("Giraffe stretches neck"); }
+	}
+
+	class Zebra {
+	    void run() { System.out.println("Zebra runs"); }
+	    void hideInStripes() { System.out.println("Zebra hides with stripes"); }
+	}
+
+	class Kangaroo {
+	    void jump() { System.out.println("Kangaroo jumps high"); }
+	    void carryBaby() { System.out.println("Kangaroo carries baby in pouch"); }
+	}
+
+	class Dolphin {
+	    void swim() { System.out.println("Dolphin swims"); }
+	    void doTrick() { System.out.println("Dolphin does a trick"); }
+	}
+
+	class Shark {
+	    void bite() { System.out.println("Shark bites"); }
+	    void smellBlood() { System.out.println("Shark smells blood"); }
+	}
+
+	class Whale {
+	    void sing() { System.out.println("Whale sings"); }
+	    void diveDeep() { System.out.println("Whale dives deep"); }
+	}
+
+	class Eagle {
+	    void fly() { System.out.println("Eagle flies high"); }
+	    void hunt() { System.out.println("Eagle hunts"); }
+	}
+
+	class Owl {
+	    void hoot() { System.out.println("Owl: Hoot!"); }
+	    void seeAtNight() { System.out.println("Owl sees at night"); }
+	}
+
+	class Snake {
+	    void hiss() { System.out.println("Snake: Ssssss!"); }
+	    void bite() { System.out.println("Snake bites"); }
+	}
+
+	class Crocodile {
+	    void snap() { System.out.println("Crocodile snaps jaws"); }
+	    void swim() { System.out.println("Crocodile swims"); }
+	}
+
